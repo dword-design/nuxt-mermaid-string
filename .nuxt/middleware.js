0b493d54c4f8a86614a5edf5c3c8603bea608249
dword <info@dword-design.de>
2021-03-22 14:31:56 +0000

    fix: fixes

--- .nuxt/middleware.js
+++
@@ -1,5 +0,0 @@
-const middleware = {}
-/* eslint-disable dot-notation */
-
-/* eslint-enable dot-notation */
-export default middleware
--- .nuxt/src.plugin.5ac41b5b.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import Component from 'vue-mermaid-string'
-
-Vue.component('VueMermaidString', Component)
