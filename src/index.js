88df6225fbe08c0e7a669193ad2c3a7ca05be0be
dword <info@sebastianlandwehr.com>
2021-11-25 01:50:53 +0000

    fix: remove client mode

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import nuxtPushPlugins from 'nuxt-push-plugins'
 
 export default function () {
-  nuxtPushPlugins(this, {
-    mode: 'client',
-    src: require.resolve('./plugin'),
-  })
+  nuxtPushPlugins(this, require.resolve('./plugin'))
 }
